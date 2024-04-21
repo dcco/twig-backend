7e076b7aefc8a9cf4ec3fb0c8635476f5f9a788f
dcco <jaehee1996@gmail.com>
2024-04-21 17:47:01 -0500

    committing starter files

---
+++ gsheet.js
@@ -0,0 +1,10 @@
+
+	// EXPRESS SERVER constants
+const express = require('express');
+const app = express();
+const PORT = 5500;
+
+	// API setup
+app.listen(PORT, () => {
+	console.log("Server started.");
+});
